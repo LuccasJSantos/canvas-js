6db7e3191a85560023b97d5d48bed00673327897
LuccasJSantos <luccasjsantos@hotmail.com>
2021-03-20 20:34:52 -0300

    :art: Add better setup for using the canvas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import Canvas from './canvas/classes/Canvas'
 import setup from './setup'
 
-/** @returns {Canvas} */
-window.init = function () {
-  // user setup goes here
-
-  return new Canvas(700, 700)
-}
-
-/** @param {Canvas} $canvas */
-window.update = function ($canvas) {
-  $canvas.setBackground()
-  
-  $canvas.setFill('red')
-  $canvas.ctx.fillRect(0, 0, 40, 40)
-}
+const canvas = new Canvas(700, 700)
 
 // setup canvas
-setup()
+setup(canvas,
+  () => {
+    // user setup goes here
+  },
+  () => {
+    // loop logic goes here (60 fps)
+  })
--- src/setup.js
+++ src/setup.js
@@ -1,12 +1,20 @@
-export default () => {
-  window.$canvas = window.init()
+import Canvas from "./canvas/classes/Canvas"
+
+/**
+ * @param {Canvas} canvas
+ * @param {function(Canvas): Canvas} initCallback
+ * @param {function(Canvas): Canvas} updateCallback
+ */
+export default (canvas, initCallback, updateCallback) => {
+  window.$canvas = canvas
 
   function update () {
     requestAnimationFrame(() => {
-      window.update($canvas)
+      updateCallback(canvas)
       update()
     })
   }
 
+  initCallback(canvas)
   update()
 }
